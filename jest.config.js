d1f38f3cece7813e03c94f2081f59cf938bfe158
eden <eden@DESKTOP-SRBRJOQ>
2022-06-20 23:41:13 +0300

    jest test can now be written in typescript

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    ".(ts|tsx)": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js"
+  ]
 };
--- src/__tests__/parsingEngineTests/parsingTests.ts
+++ src/__tests__/parsingEngineTests/parsingTests.ts
@@ -1,14 +1,22 @@
 import { subArray } from "../../parsingEngine/parsingServiceUtils";
 
+export function CompareArrays<T>(left: T[] , right: T[]): boolean {
+    if(left === null && right === null) return true;
+    if(left === null && right !== null) return false;
+    if(left !== null && right === null) return false;
+    if(left.length !== right.length) return false;
+    for(let i = 0 ; i < left.length ; i++){
+        if(left[i] !== right[i]) return false;
+    }
+    return true;
+}
+
 describe('subArray() tests' , () => {
     test('success', () => {
         const source = [1,2,3,4,5,6];
-        const result = subArray(source,3,5);
-        expect(result.length).toBe(3);
-        expect(result[0]).toBe(4);
-        expect(result[1]).toBe(5);
-        expect(result[2]).toBe(6);
-        
+        const result : number[] = subArray(source,3,5);
+        const expected : number[] = [4,5,6];
+        expect(CompareArrays(result,expected)).toBe(true);   
     })
 })
 
